d0f6c89d21dd989fe697d98f599abcdc0deba2cf
George Iulian Diaconu <kuhlin@gmail.com>
2022-06-21 19:00:19 +0200

    adding routes

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,4 +18,10 @@ app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore 
 
 app.MapGet("/", () => "Hello World!");
 
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 app.Run();

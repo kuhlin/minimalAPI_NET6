ed8ef4d5cc1473fe4dd1fe76c8cdde2226aec114
George Iulian Diaconu <kuhlin@gmail.com>
2022-06-30 13:42:14 +0200

    adding in memory database

--- PizzaStore/Db.cs
+++ PizzaStore/Db.cs
@@ -1,46 +1,46 @@
-namespace PizzaStore.DB; 
+namespace PizzaStore.DB;
 
- public record Pizza 
+ public record Pizza
  {
-   public int Id {get; set;} 
+   public int Id {get; set;}
    public string ? Name { get; set; }
  }
 
- public class PizzaDB
+ public static class PizzaDB
  {
-   private static List<Pizza> _pizzas = new List<Pizza>()
+   private static List<Pizza> _pizzas = new()
    {
      new Pizza{ Id=1, Name="Montemagno, Pizza shaped like a great mountain" },
      new Pizza{ Id=2, Name="The Galloway, Pizza shaped like a submarine, silent but deadly"},
-     new Pizza{ Id=3, Name="The Noring, Pizza shaped like a Viking helmet, where's the mead"} 
+     new Pizza{ Id=3, Name="The Noring, Pizza shaped like a Viking helmet, where's the mead"}
    };
 
-   public static List<Pizza> GetPizzas() 
+   public static List<Pizza> GetPizzas()
    {
      return _pizzas;
-   } 
+   }
 
-   public static Pizza ? GetPizza(int id) 
+   public static Pizza ? GetPizza(int id)
    {
      return _pizzas.SingleOrDefault(pizza => pizza.Id == id);
-   } 
+   }
 
-   public static Pizza CreatePizza(Pizza pizza) 
+   public static Pizza CreatePizza(Pizza pizza)
    {
      _pizzas.Add(pizza);
      return pizza;
    }
 
-   public static Pizza UpdatePizza(Pizza update) 
+   public static Pizza UpdatePizza(Pizza update)
    {
-     _pizzas = _pizzas.Select(pizza =>
+     _pizzas = _pizzas.ConvertAll(pizza =>
      {
        if (pizza.Id == update.Id)
        {
          pizza.Name = update.Name;
        }
        return pizza;
-     }).ToList();
+     });
      return update;
    }
 
